b59468ebe37aecde2b09cad01eee1151bc5d1a11
AgentWong <hwong@hermanwong.net>
2019-08-14 16:22:15 -1000

    Create Test-Softwarev2.ps1

---
+++ Test-Softwarev2.ps1
@@ -0,0 +1,68 @@
+<#
+Gets computer objects from AD.  Then uses a for loop to check for presence of software folder and a file
+that the software generates when it is activated.  The output will then be exported to a CSV file for easy organizing.
+#>
+
+#Gets the current user account running the script.
+$CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+#Checks if the account running has certain text in its name, if not, it will relaunch the script as an administrator.
+if ($CurrentUser -notlike "*z0*") {
+    # Self-elevate the script if required
+    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+        Exit
+    }
+} #Self-elevate check.
+
+$Progress = 0
+$SearchBase = "MyOU"
+$Computers = Get-ADComputer -Filter * -SearchBase $SearchBase -Properties LastLogonDate | Sort-Object Name
+$Count = $Computers.Count
+$Date = Get-Date -f MM-dd-yyyy
+
+#Custom array to store the ouput.
+$Store = @()
+
+$Computers | ForEach-Object {
+    $HostName = $Null
+    $NameMatch = $Null
+    $Software = $Null
+    $LogonDate = $_.LastLogonDate
+
+    #Tests to see if the computer is pingable.
+    $Online = Test-NetConnection -ComputerName $_.Name -InformationLevel Detailed
+    if($Online.PingSucceeded -eq $True){
+        $HostName = Get-WmiObject -ComputerName $Online.RemoteAddress -ClassName Win32_ComputerSystem -Property Name | Select-Object -ExpandProperty Name
+        $NameMatch = $_.Name -eq $HostName
+        Start-Sleep -Seconds 2
+        $Avamar = $null -eq (Get-WmiObject -ComputerName $Online.RemoteAddress -ClassName CIM_DataFile -Filter "drive='C:' AND path='\\Program Files\\Software\\var\\' AND extension='cfg'")
+    }
+    else
+    {
+        $HostName = "Not Online."
+        $Avamar = "Not Online."
+    } #Close Ping Test.
+
+    #Creates a custom Powershell Object to append to the storage array.
+    $store += New-Object PSObject -Property @{
+    'Computer' = $_.Name
+    'DNS' = $Online.NameResolutionSucceeded
+    'Ping' = $Online.PingSucceeded
+    'LogonDate' = $LogonDate
+    'IPAddress' = $Online.RemoteAddress
+    'Software' = $Software
+    'HostName' = $HostName
+    'NameMatch' = $NameMatch
+    } #Close custom PSObject Hash Table.
+    Start-Sleep -Seconds 2
+
+    #Tracks progress as it goes through each loop.
+    $Progress++
+    Write-Progress -Activity "Checking Software status..." -Status "Checking: $Progress of $($Count)" -PercentComplete (($Progress / $Count) * 100) -Id 1
+} #Close For-Each Loop.
+
+#Exports the output from the array into a CSV file.
+$Store | Select-Object Computer,NameMatch,HostName,DNS,Ping,LogonDate,IPAddress,Software |
+Export-Csv "C:\DRV\Scripts\Reports\SoftwareStatus_$Date.csv" -NoTypeInformation
